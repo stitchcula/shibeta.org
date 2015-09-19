7a531c0a84209c9799d0dbdb4edb1d943604b2b7
stitch cula <1132463097@qq.com>
2015-09-20 04:41:39 +0800

    v0.1.4rc2
    Created self sync.

--- task.js
+++ task.js
@@ -2,20 +2,27 @@ var schedule = require('node-schedule')
     ,co=require('co')
     ,redis=require('co-redis')(require('redis').createClient(6379,'121.42.51.112'))
     ,fs=require('co-fs')
+    //,thunk=require('thunkify')
+    ,exec=require('child_process').exec
+    //,fns=require('fns.js')
 
 var fast=schedule.scheduleJob('*/30 * * * * *',function(){
     co(function*(){
-        while(1){
-            var task=JSON.parse(yield redis.rpoplpush('fastTask','fastTaskLog'))
-            if(task)
-                switch(task.type){
+        while(1) {
+            var task = JSON.parse(yield redis.rpoplpush('fastTask', 'fastTaskLog'))
+            console.log(new Date())
+            if (task){
+                console.log(task)
+                switch (task.type) {
                     case 'sync':
-                        fns.selfSync(task.content)
+                        exec('/ext/shibeta/sync.sh',{cwd:'/ext/shibeta/'},function(err,stdout,stderr){
+                            console.log(stdout)
+                        })
                         break
                     default:
                         eval(task.content)
                 }
-            else break
+            }else break
         }
     })
 })
